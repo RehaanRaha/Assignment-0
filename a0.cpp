89240beaff05dab9879b6d22aaeaac7411260333
RehaanRaha <rehaan.raha_asp25@ashoka.edu.in>
2024-09-07 01:06:25 +0530

    first commit

---
+++ a0.cpp
@@ -0,0 +1,9 @@
+// this is a sample template file, in C++
+// All this file does is print out the result mentioned between '<<' and '>>'
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World" << std::endl;
+    // Instead of '...', write "Hello World" within quotes
+    return 0;
+}
